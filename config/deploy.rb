8a05f7db3cfe895c4e57185306a75aba8f75f570
Evgeny Lapin <lev@openteam.ru>
2016-02-29 17:07:25 +0600

    fetch linked_files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 require 'openteam/capistrano/deploy'
 
-set :linked_files, %w[
+set :linked_files, fetch(:linked_files) + %w[
   public/NASA-space-time-lapse-clips-HD.flv
   public/NASA-space-time-lapse-clips-HD.mp4
   public/NASA-space-time-lapse-clips-HD.ogv
