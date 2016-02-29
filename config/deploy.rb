8d5fab7e07cb362f6827718adf892ae8641c9033
Evgeny Lapin <lev@openteam.ru>
2016-02-29 17:04:10 +0600

    use video files from public

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,8 @@
 require 'openteam/capistrano/deploy'
 
-set :linked_files, %w{ public/NASA-space-time-lapse-clips-HD.* }
+set :linked_files, %w[
+  public/NASA-space-time-lapse-clips-HD.flv
+  public/NASA-space-time-lapse-clips-HD.mp4
+  public/NASA-space-time-lapse-clips-HD.ogv
+  public/NASA-space-time-lapse-clips-HD.webm
+]
