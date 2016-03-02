d77216649682cc6bbeeef616ddd871cb0a16a923
Evgeny Lapin <lev@openteam.ru>
2016-03-02 14:54:36 +0600

    set :bundle_binstubs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,7 @@
 require 'openteam/capistrano/deploy'
 
+set :bundle_binstubs, -> { shared_path.join('bin') }
+
 set :linked_files, fetch(:linked_files) + %w[
   public/NASA-space-time-lapse-clips-HD.flv
   public/NASA-space-time-lapse-clips-HD.mp4
